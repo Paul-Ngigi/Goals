ebfcce8ca8eea9457b0f5e35fd4023b6ca2fa34e
Paul-Ngigi <paulkush7777@gmail.com>
2021-03-31 11:36:31 +0300

    add services to the site

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,6 +1,7 @@
-import { FnParam } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
-import { Goal } from '../goal'
+import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+import { AlertService } from '../alert-service/alert.service'
 
 @Component({
   selector: 'app-goal',
@@ -9,14 +10,8 @@ import { Goal } from '../goal'
 })
 export class GoalComponent implements OnInit {
 
-  goals: Goal[] = [
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
-    new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
-    new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
-  ];
+  goals!: Goal[];
+  alertService!: AlertService;
 
   toggleDetail(index: number) {
     this.goals[index].showDescription = !this.goals[index].showDescription;
@@ -26,19 +21,23 @@ export class GoalComponent implements OnInit {
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
 
-      if (toDelete){
-        this.goals.splice(index,1);
+      if (toDelete) {
+        this.goals.splice(index, 1);
+        this.alertService.alertMe("Your goal has been deleted")
       }
     }
   }
 
-  addNewGoal(goal: any){
+  addNewGoal(goal: any) {
     let goalLength = this.goals.length;
-    goal.id = goalLength+1;
+    goal.id = goalLength + 1;
     goal.completeDate = new Date(goal.completeDate)
     this.goals.push(goal)
-  } 
-  constructor() { }
+  }
+  constructor(goalService: GoalService, alertService: AlertService) {
+    this.goals = goalService.getGoals();
+    this.alertService = alertService;
+  }
 
   ngOnInit(): void {
   }
